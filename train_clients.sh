01a5af7670a4873044e620e545e1fb3eca629667
Obaidullah Zaland <obaidullahzaland1997@gmail.com>
2025-03-06 01:21:26 +0100

    data division and add client specific data loading

---
+++ train_clients.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#SBATCH --gpus 1
+#SBATCH -A Berzelius-2025-50
+#SBATCH -t 2-00:00:00
+#SBATCH --mail-type=END,FAIL
+#SBATCH --mail-user obaidullah.zaland@umu.se
+
+python code_adapt/main_fl.py referit_adapt_afsfpnvisnew5 --ds_to_use='flickr30k' --mdl_to_use='shuffleprune' --bs=1 --resume=False --client 4
+python code_adapt/main_fl.py referit_adapt_afsfpnvisnew4 --ds_to_use='flickr30k' --mdl_to_use='shuffleprune' --bs=1 --resume=False --client 3
+python code_adapt/main_fl.py referit_adapt_afsfpnvisnew3 --ds_to_use='flickr30k' --mdl_to_use='shuffleprune' --bs=1 --resume=False --client 2
+python code_adapt/main_fl.py referit_adapt_afsfpnvisnew2 --ds_to_use='flickr30k' --mdl_to_use='shuffleprune' --bs=1 --resume=False --client 1
+python code_adapt/main_fl.py referit_adapt_afsfpnvisnew1 --ds_to_use='flickr30k' --mdl_to_use='shuffleprune' --bs=1 --resume=False --client 0
