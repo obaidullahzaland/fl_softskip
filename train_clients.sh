f5775847ef7229e7197740c82d0d98b61ef284d1
Obaidullah Zaland <obaidullahzaland1997@gmail.com>
2025-03-06 19:27:20 +0100

    change validaiton file from fixed to kitchen to client specific file

--- train_clients.sh
+++ train_clients.sh
@@ -5,8 +5,8 @@
 #SBATCH --mail-type=END,FAIL
 #SBATCH --mail-user obaidullah.zaland@umu.se
 
-python code_adapt/main_fl.py referit_adapt_afsfpnvisnew5 --ds_to_use='flickr30k' --mdl_to_use='shuffleprune' --bs=16 --resume=False --client 4
-python code_adapt/main_fl.py referit_adapt_afsfpnvisnew4 --ds_to_use='flickr30k' --mdl_to_use='shuffleprune' --bs=2 --resume=False --client 3
-python code_adapt/main_fl.py referit_adapt_afsfpnvisnew3 --ds_to_use='flickr30k' --mdl_to_use='shuffleprune' --bs=2 --resume=False --client 2
-python code_adapt/main_fl.py referit_adapt_afsfpnvisnew2 --ds_to_use='flickr30k' --mdl_to_use='shuffleprune' --bs=2 --resume=False --client 1
-python code_adapt/main_fl.py referit_adapt_afsfpnvisnew1 --ds_to_use='flickr30k' --mdl_to_use='shuffleprune' --bs=2 --resume=False --client 0
+# python code_adapt/main_fl.py referit_adapt_afsfpnvisnew5 --ds_to_use='flickr30k' --mdl_to_use='shuffleprune' --bs=16 --resume=False --client 4
+python code_adapt/main_fl.py referit_adapt_afsfpnvisnew4 --ds_to_use='flickr30k' --mdl_to_use='shuffleprune' --bs=16 --resume=False --client 3
+python code_adapt/main_fl.py referit_adapt_afsfpnvisnew3 --ds_to_use='flickr30k' --mdl_to_use='shuffleprune' --bs=16 --resume=False --client 2
+python code_adapt/main_fl.py referit_adapt_afsfpnvisnew2 --ds_to_use='flickr30k' --mdl_to_use='shuffleprune' --bs=16 --resume=False --client 1
+python code_adapt/main_fl.py referit_adapt_afsfpnvisnew1 --ds_to_use='flickr30k' --mdl_to_use='shuffleprune' --bs=16 --resume=False --client 0
