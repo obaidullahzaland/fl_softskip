cdbd062dd27907ee5fb7d30d566b907eec255c1a
Obaidullah Zaland <obaidullahzaland1997@gmail.com>
2025-03-11 23:53:36 +0100

    test file

--- test.py
+++ test.py
@@ -9,7 +9,7 @@ transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.5
 train_dataset = datasets.CIFAR10(root='../data', train=True, download=True, transform=transform)
 train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=32, shuffle=True)
 
-    test_dataset = datasets.CIFAR10(root='../data', train=False, download=True, transform=transform)
+test_dataset = datasets.CIFAR10(root='../data', train=False, download=True, transform=transform)
 test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=32, shuffle=False)
 
 class Head(nn.Module):
